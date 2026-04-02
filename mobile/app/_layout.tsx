dd1abd1b9a47d51751a00ccbfba9e6d06ca2e591
Samuel BLARD <contact@samuelblard.com>
2026-04-02 11:08:56 +0200

    refactor: remove global keyboard dismissal wrapper from root layout

--- mobile/app/_layout.tsx
+++ mobile/app/_layout.tsx
@@ -2,7 +2,7 @@ import { useEffect } from 'react';
 import { Stack } from 'expo-router';
 import { StatusBar } from 'expo-status-bar';
 import { GestureHandlerRootView } from 'react-native-gesture-handler';
-import { StyleSheet, Keyboard, TouchableWithoutFeedback, View } from 'react-native';
+import { StyleSheet } from 'react-native';
 import { useFrameworkReady } from '../src/hooks/useFrameworkReady';
 import { AppProvider } from '../src/context/AppContext';
 
@@ -12,22 +12,19 @@ export default function RootLayout() {
   return (
     <GestureHandlerRootView style={styles.container}>
       <AppProvider>
-        <TouchableWithoutFeedback onPress={Keyboard.dismiss} accessible={false}>
-          <View style={styles.container}>
-            <Stack screenOptions={{ headerShown: false }}>
-              <Stack.Screen name="index" options={{ title: 'My Movie Rankings' }} />
-              <Stack.Screen
-                name="list/[id]"
-                options={{
-                  headerShown: false,
-                  presentation: 'card',
-                  animation: 'slide_from_right',
-                }}
-              />
-            </Stack>
-            <StatusBar style="light" />
-          </View>
-        </TouchableWithoutFeedback>
+        {/* On a retiré le TouchableWithoutFeedback et sa View englobante */}
+        <Stack screenOptions={{ headerShown: false }}>
+          <Stack.Screen name="index" options={{ title: 'My Movie Rankings' }} />
+          <Stack.Screen
+            name="list/[id]"
+            options={{
+              headerShown: false,
+              presentation: 'card',
+              animation: 'slide_from_right',
+            }}
+          />
+        </Stack>
+        <StatusBar style="light" />
       </AppProvider>
     </GestureHandlerRootView>
   );
